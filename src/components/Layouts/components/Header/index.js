f0e7091334cb617b09eed06aa4d113b63d507252
Nghia <nghia.nguyenmysterios@hcmut.edu.vn>
2023-08-07 16:54:57 +0700

    update header search

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,34 +1,66 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-
+import React, { useEffect, useState } from 'react';
+import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
+
 import styles from './Header.module.scss'
 import images from '~/assets/images';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import SearchItem from '~/components/SearchItem';
+
 
 const cx = classNames.bind(styles);
 
 
 function Header() {
-    
+    const [searchResult, setSearchResult] = useState([]);
+
+    useEffect(() => {
+        // * handle search result to visible tippy
+        setTimeout(() => {
+            setSearchResult([1, 2]);
+        }, 2000);
+    }, [])
+
     return ( 
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <div className={cx('logo')}>
                     <img src={images.logo} alt='Tiktok'/>
                 </div>
-                <div className={cx('search')}>
-                    <input 
-                        placeholder='Search accounts and videos'
-                        spellCheck={false}
-                    />
-                    <button className={cx('clear-btn')}>
-                        <FontAwesomeIcon icon={faCircleXmark}/>
-                    </button>
-                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
-                    </button>
-                </div>
+                <Tippy 
+                    interactive
+                    visible={searchResult.length > 0}
+                    render={attrs => (
+                        <div className={cx('search-results')} tabIndex="-1" {...attrs}>
+                                <PopperWrapper>
+                                    <h4 className={cx('search-label')}>
+                                        Results
+                                    </h4>
+                                    <SearchItem/>
+                                    <SearchItem/>
+                                    <SearchItem/>
+                                </PopperWrapper>
+                            </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input 
+                            placeholder='Search accounts and videos'
+                            spellCheck={false}
+                        />
+                        <button className={cx('clear-btn')}>
+                            <FontAwesomeIcon icon={faCircleXmark}/>
+                        </button>
+                        
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>
+    
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                        </button>
+                    </div>
+                </Tippy>
                 <div className='actions'>
                     {/* action */}
                 </div>
---
+++ src/components/SearchItem/index.js
@@ -0,0 +1,23 @@
+import classNames from "classnames/bind";
+import styles from './SearchItem.module.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+
+const cx = classNames.bind(styles) 
+
+function SearchItem({ children }) {
+    return ( 
+        <div className={cx('wrapper') }>
+            <img className={cx('avatar')} src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/7103407749668536322.jpeg?x-expires=1691571600&x-signature=GrnhoTk7opbYmbwLGsxUfIvf64A%3D" alt="img" />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Kamisato Myst</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}/>
+                </h4>
+                <span className={cx('username')}>kxmyst</span>
+            </div>
+        </div>
+    );
+}
+
+export default SearchItem;
